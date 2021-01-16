f3b97408a667fabf90691d31b9709254787b17fd
unknown <nogir16111998@gmail.com>
2021-01-16 13:52:26 +0700

    mantap

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class Credits : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class Credits : MonoBehaviour
 	{
 		Debug.Log("QUIT");
 		Application.Quit();
+	}
+	
+	public void StartGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 	}
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,5 +10,12 @@ public class GameManager : MonoBehaviour
     public void CompleteLevel()
 	{
 		completeLevelUI.SetActive(true);
+		StartCoroutine(ChangePage());
+	}
+	
+	public IEnumerator ChangePage()
+	{
+		yield return new WaitForSeconds(2f);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 }
