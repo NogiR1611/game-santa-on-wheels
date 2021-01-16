1a60fe1c10c9d296a22607a2851b7cbb571a8ae8
unknown <nogir16111998@gmail.com>
2021-01-16 12:54:01 +0700

    fina

---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VehicleController : MonoBehaviour
+{
+	public static VehicleController instance;
+	public Rigidbody2D FTire;
+	public Rigidbody2D BTire;
+	public float speed;
+	public float movement;
+	public float gas = 1f;
+	public float gasConsumption = 0.1f;
+    public Image gasImage;
+	// Start is called before the first frame update
+    public void Awake()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+	}
+	
+	void Start()
+    {
+        
+    }
+	
+	private void Update()
+	{
+		movement = Input.GetAxis("Horizontal");
+		gasImage.fillAmount = gas;
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+		if (gas > 0)
+		{
+			FTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+			BTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+		}
+		gas = gas - gasConsumption * Time.fixedDeltaTime * Mathf.Abs(movement);
+	}
+}
---
+++ Assets/Scripts/gasRefill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gasRefill : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		VehicleController.instance.gas = 1f;
+		Destroy(gameObject);
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
